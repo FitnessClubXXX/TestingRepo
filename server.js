50ca53faf11401f4b36f36da4e0d325faaef99d0
Patryk Stronski <p.stronski13@gmail.com>
2020-01-06 17:51:59 +0100

    added test and fx

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ app.get('/', (req,res) => {
 	console.log('got /');
 	res.send('Thanks, this is prototype for fitness club XXX');
 });
+
+app.get('/calc', (req,res) => {
+	res.send(calculate(req.query.a, req.query.b));
+});
+
+function calculate(a, b) { 
+	return a+b;
+}
+module.exports = { calculate }
--- test.spec.js
+++ test.spec.js
@@ -1,7 +1,14 @@
 const expect = require('chai').expect;
+const s = require('./server.js');
 
 describe('should pass', () => {
 	it('should pass', () => {
 		expect(true).to.equal(true);
+	});
+});
+
+describe('test calc', () => {
+	it('shoudl give 4', () => {
+		expect(s.calculate(2,2)).to.equal(4);
 	});
 });
